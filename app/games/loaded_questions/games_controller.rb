93c13882b031400a0ac899ac1927bfa49bfb038b
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-02-01 15:43:37 -0500

    add routes

---
+++ app/games/loaded_questions/games_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module LoadedQuestions
+  class GamesController < ::ApplicationController
+    # GET /loaded_questions/games/new
+    def new
+    end
+
+    # POST /loaded_questions/games/create
+    def create
+    end
+
+    # GET /loaded_questions/games/:id
+    def show
+    end
+  end
+end

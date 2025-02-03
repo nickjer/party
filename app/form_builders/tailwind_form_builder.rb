db614f2350f1bef4705d91bb43d3fbc7afe4a1f4
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-02-02 19:34:59 -0500

    add error styling

--- app/form_builders/tailwind_form_builder.rb
+++ app/form_builders/tailwind_form_builder.rb
@@ -16,6 +16,12 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     text-gray-900
   ].join(" ").freeze
 
+  LABEL_ERROR_STYLE = %w[
+    text-sm
+    font-medium
+    text-red-600
+  ].join(" ").freeze
+
   SUBMIT_STYLE = %w[
     bg-blue-500
     text-white
@@ -34,23 +40,42 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     p-2.5
   ].join(" ").freeze
 
+  TEXT_FIELD_ERROR_STYLE = %w[
+    bg-gray-50
+    border
+    border-red-500
+    text-red-900
+    text-sm
+    rounded-lg
+    p-2.5
+  ].join(" ").freeze
+
   def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
     options[:class] = "#{CHECK_BOX_STYLE} #{options[:class]}"
     super(method, options, checked_value, unchecked_value)
   end
 
   def label(method, text = nil, options = {}, &block)
+    style = error?(method) ? LABEL_ERROR_STYLE : LABEL_STYLE
     if text.is_a?(Hash)
-      text[:class] = "#{LABEL_STYLE} #{text[:class]}"
+      text[:class] = "#{style} #{text[:class]}"
     else
-      options[:class] = "#{LABEL_STYLE} #{options[:class]}"
+      options[:class] = "#{style} #{options[:class]}"
     end
     super(method, text, options, &block)
   end
 
   def text_field(method, options = {})
-    options[:class] = "#{TEXT_FIELD_STYLE} #{options[:class]}"
-    super(method, options)
+    style = error?(method) ? TEXT_FIELD_ERROR_STYLE : TEXT_FIELD_STYLE
+    options[:class] = "#{style} #{options[:class]}"
+    output = super(method, options)
+    if error?(method)
+      error = @template.content_tag(
+        :p, @object.errors[method], class: "text-sm text-red-600 mt-1"
+      )
+      output.concat(error)
+    end
+    output
   end
 
   def text_area(method, options = {})
@@ -71,5 +96,11 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
   def submit(value = "Submit", options = {})
     options[:class] = "#{SUBMIT_STYLE} #{options[:class]}"
     super(value, options)
+  end
+
+  private
+
+  def error?(method)
+    @object.errors[method].present?
   end
 end

593f68924b272f9d7c5ef414414da5816c14836f
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-02-03 08:52:58 -0500

    some clean up

---
+++ test/games/loaded_questions/games_controller_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+module LoadedQuestions
+  class GamesControllerTest < ActionDispatch::IntegrationTest
+    test "should get new" do
+      get loaded_questions_games_new_url
+      assert_response :success
+    end
+
+    test "should get create" do
+      get loaded_questions_games_create_url
+      assert_response :success
+    end
+
+    test "should get show" do
+      get loaded_questions_games_show_url
+      assert_response :success
+    end
+  end
+end

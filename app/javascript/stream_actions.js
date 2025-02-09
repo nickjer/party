6ddbf4f53bba76e4739b1c5dbe24b8b83cd7f162
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-02-08 20:41:33 -0500

    simplify reload action

--- app/javascript/stream_actions.js
+++ app/javascript/stream_actions.js
@@ -1,11 +1,7 @@
 Turbo.StreamActions.reload = function () {
   this.targetElements.forEach((targetElement) => {
-    if (targetElement.src) {
-      targetElement.disabled = false
-      targetElement.reload()
-      targetElement.disabled = true
-    } else if (targetElement.dataset.src) {
-      targetElement.src = targetElement.dataset.src
-    }
+    targetElement.removeAttribute("disabled")
+    targetElement.reload()
+    targetElement.setAttribute("disabled", "")
   })
 }

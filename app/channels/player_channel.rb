4aa80960e6a7b9a749f27dada8445c95a9add7b8
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-10-17 19:08:19 -0400

    initial work on connection/disconnection broadcasters

--- app/channels/player_channel.rb
+++ app/channels/player_channel.rb
@@ -23,19 +23,36 @@ class PlayerChannel < ApplicationCable::Channel
   def subscribed
     if (stream_name = verified_stream_name_from_params)
       @player = GlobalID.find(stream_name) || raise("Invalid player GID")
-
-      ::PlayerConnections.instance.increment(player.id)
-      game.broadcast_reload_players
-
       stream_from stream_name
+
+      count = ::PlayerConnections.instance.increment(player.id)
+      return if count > 1
+
+      broadcaster =
+        case game_kind
+        when :loaded_questions
+          LoadedQuestions::Broadcast::PlayerConnected.new(player_id: player.id)
+        else
+          raise "Unknown game kind: #{game_kind.inspect}"
+        end
+      broadcaster.call
     else
       reject
     end
   end
 
   def unsubscribed
-    ::PlayerConnections.instance.decrement(player.id)
-    game.broadcast_reload_players
+    count = ::PlayerConnections.instance.decrement(player.id)
+    return if count.positive?
+
+    broadcaster =
+      case game_kind
+      when :loaded_questions
+        LoadedQuestions::Broadcast::PlayerDisconnected.new(player_id: player.id)
+      else
+        raise "Unknown game kind: #{game_kind.inspect}"
+      end
+    broadcaster.call
   end
 
   private
@@ -43,5 +60,5 @@ class PlayerChannel < ApplicationCable::Channel
   # @dynamic player
   attr_reader :player
 
-  def game = player.game
+  def game_kind = @game_kind = player.game.kind
 end

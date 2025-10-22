47e390a7093318350e64f35e58fec235f61ebe73
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-10-22 19:52:13 -0400

    add home controller test

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "#index renders home page successfully" do
+    get root_path
+
+    assert_response :success
+  end
+
+  test "#index displays Loaded Questions game card" do
+    get root_path
+
+    assert_response :success
+    assert_dom "h2", text: "Loaded Questions"
+    assert_dom "a[href='#{new_loaded_questions_game_path}']",
+      text: /Start New Game/
+  end
+
+  test "#index displays Burn Unit game card" do
+    get root_path
+
+    assert_response :success
+    assert_dom "h2", text: "Burn Unit"
+    assert_dom ".btn.disabled", text: /Coming Soon/
+  end
+
+  test "#index displays page title" do
+    get root_path
+
+    assert_response :success
+    assert_dom "h1", text: /Party/
+  end
+end

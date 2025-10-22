484982c04c717124a2f73cd11118ee9cdb229b46
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-10-22 07:09:19 -0400

    fix factories

--- test/factories/lq_games.rb
+++ test/factories/lq_games.rb
@@ -10,39 +10,34 @@ FactoryBot.define do
 
     trait :with_guesser do
       transient do
+        guesser_user { association :user }
         guesser_name { Faker::Name.unique.first_name.ljust(3, "a") }
-        guesser do
-          association(:lq_player, game: instance, name: guesser_name,
-            guesser: true, strategy: :build)
-        end
       end
 
       after(:build) do |game, context|
-        game.instance_eval { cached_players << context.guesser }
+        build(:lq_player, game:, user: context.guesser_user,
+          name: context.guesser_name, guesser: true)
       end
     end
 
     trait :with_players do
       transient do
-        player_names { Array.new(2) { Faker::Name.unique.first_name.ljust(3, "a") } }
+        player_names do
+          Array.new(2) { Faker::Name.unique.first_name.ljust(3, "a") }
+        end
         players { player_names.map { |name| { name:, answer: nil } } }
+        users { players.map { association(:user) } }
       end
 
       after(:build) do |game, context|
-        context.players.each do |player_data|
-          player = build(:lq_player,
-            game:,
-            name: player_data.fetch(:name),
-            guesser: false,
-            answer: player_data.fetch(:answer))
-          game.instance_eval { cached_players << player }
+        context.players.zip(context.users).each do |player_data, user|
+          build(:lq_player, game:, user:, name: player_data.fetch(:name),
+            guesser: false, answer: player_data.fetch(:answer))
         end
       end
     end
 
     trait :with_answers do
-      with_players
-
       transient do
         players do
           player_names.map do |name|
@@ -50,19 +45,19 @@ FactoryBot.define do
           end
         end
       end
+
+      with_players
     end
 
     factory :lq_polling_game, traits: %i[with_guesser with_players]
 
-    factory :lq_matching_game,
-      traits: %i[with_guesser with_players with_answers] do
+    factory :lq_matching_game, traits: %i[with_guesser with_answers] do
       after(:build) do |game|
         LoadedQuestions::BeginGuessingRound.new(game:).call
       end
     end
 
-    factory :lq_completed_game,
-      traits: %i[with_guesser with_players with_answers] do
+    factory :lq_completed_game, traits: %i[with_guesser with_answers] do
       after(:build) do |game|
         LoadedQuestions::BeginGuessingRound.new(game:).call
         LoadedQuestions::CompleteRound.new(game:).call
--- test/factories/lq_players.rb
+++ test/factories/lq_players.rb
@@ -2,22 +2,15 @@
 
 FactoryBot.define do
   factory :lq_player, class: "LoadedQuestions::Player" do
-    user { association :user, strategy: :create }
+    user
     name { Faker::Name.unique.first_name.ljust(3, "a") }
     guesser { false }
     answer { nil }
     game { association :lq_game }
 
     initialize_with do
-      player = LoadedQuestions::Player.build(
-        game_id: game.id,
-        user_id: user.id,
-        name:,
-        guesser:
-      )
-
+      player = game.add_player(user_id: user.id, name:, guesser:)
       player.answer = NormalizedString.new(answer) if answer.present?
-
       player
     end
 
